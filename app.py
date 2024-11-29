80ac36ccaee4758e42a64f57e70730049681e830
Betovoley <beto_voley@hotmail.com>
2024-11-29 09:48:40 -0300

    priemr commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+def main():
+    st.title("Bem-vindo a Betovoley-Sada Cruzeiro Stats")
+
+
+if __name__ == "__main__":
+    main()
