cf5cb5c0b8f7c4aa13167b2a2553cf77b0ebd14c
Betovoley <beto_voley@hotmail.com>
2024-12-03 14:33:15 -0300

    csv nuevos, comenzando importación y limpieza de datos saque_season_24.csv

--- pages/1_analisis_equipo_basico.py
+++ pages/1_analisis_equipo_basico.py
@@ -4,5 +4,7 @@ def main():
     st.title("Análisis por equipo básico")
     st.write("Aquí irá el contenido relacionado con el análisis básico de equipo.")
 
+
+
 if __name__ == "__main__":
     main()
